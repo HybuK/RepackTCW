4dcca2eafa5c30d588043316ee167fc5d2779490
HybuK <i7oxep@yandex.ru>
2020-07-03 05:24:59 +0700

    add release

--- wolfsp_i386.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd '~/_games/RepackTCW/' //change directory to executable file
-exec ./wolfsp.x86 +set sv_cheats 1
--- wolfsp_x64.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd '~/_games/RepackTCW/' //change directory to executable file
-exec ./wolfsp.x86_64 +set sv_cheats 1
